cb630d79e03d604a11d4a22e72f59ef8f3e5951e
Iman974 <reyan.toumi@gmail.com>
2023-01-31 16:57:23 +0100

    Added market and connected and sync with homes

--- home.py
+++ home.py
@@ -3,6 +3,7 @@ from multiprocessing import Process, Event, Lock, current_process
 import time
 import signal
 from random import randrange, random
+import socket
 
 HOMES_COUNT = 5
 MIN_CONSUMPTION_RATE = 1
@@ -10,6 +11,10 @@ MIN_PRODUCTION_RATE = 1
 MAX_CONSUMPTION_RATE = 3
 MAX_PRODUCTION_RATE = 3
 
+LOCALHOST = "localhost"
+MARKET_PORT = 9000
+
+# Enum-like class
 class Policy:
     GIVE_ONLY = "GIVE_ONLY"
     SELL_ONLY = "SELL_ONLY"
@@ -32,16 +37,15 @@ sync_event = Event()
 next_tick = Event()
 
 # TODO LIST
-# - replace the TRADE_POLICY strings with vars to avoid mistakes (?)
-# - catch SIGINT to close msq queue
 # - Handle the SIGINT error if it is stuck on semaphore or event (check docs) when simulation is stopped
-# - Make sure that no lock acquire/release is missing in MsgQueueCounter
 # - Sell on market the remaining energy that was not taken by needy homes in SELL_IF_NONE policy
+# - Remove next_tick event if only used by parent process and replace in on_tick_start callback.
+# well actually it's not that bad to put tick updates with other processes in parent process...
+# - Clean file from debug print
+# - Move all home-related only variables to Home class
 
 KEY = 257
 msg_queue = MessageQueue(KEY, flags=IPC_CREAT)
-
-timestamp = 0
 
 # weather hard-coded
 temperature = 25 # °C
@@ -58,54 +62,55 @@ def handle_interrupt(*args):
     for h in homes:
         h.terminate()
     msg_queue.remove()
-    print("-----------------END OF SIMULATION--------------------")
+    tick_socket.close()
+    
+    print("\n-----------------END OF SIMULATION--------------------")
     exit(0)
 
 # Allows for home to sync. Calling this each round ensures the ticks do not mix,
 # i.e. tick N (present) is not mixed with ticks N-1 (past) or N+1 (future).
 # The on_sync function will be called only once, just before releasing all processes.
-def sync_all_homes(on_sync = (lambda: None)):
+def sync_all_homes(on_sync_callback = (lambda: None), *args):
     if atomic_counter.increment(1) == HOMES_COUNT:
         atomic_counter.setValue(0)
-        on_sync()
+        on_sync_callback(*args)
         sync_event.set()
     else:
         sync_event.wait()
     sync_event.clear()
 
-def on_tick_end():
-    next_tick.set()
+def on_tick_start():
     # We assume that there will be no takers or givers by default
     givers_advertisment.set()
     awaiting_takers.set()
 
     time.sleep(1)
+    next_tick.set()
 
 #---------------------------------------------------------------------
 
 # Represents an atomic countdown stored in a message queue.
 class MsgQueueCounter:
 
-    # Type of the message used to store the counter, must be unused by other interactions with the queue
-    MSG_TYPE = 999
-
     # Msg queue must be opened prior to instanciation
-    def __init__(self, start_value, queue):
+    # Type of the message used to store the counter, must be unused by other interactions with the queue
+    def __init__(self, start_value, queue, msg_type):
         self.value = start_value
         self.start_value = start_value
         self.msg_queue = queue
+        self.msg_type = msg_type
 
-        self.msg_queue.send(str(start_value).encode(), type=self.MSG_TYPE)
+        self.msg_queue.send(str(start_value).encode(), type=self.msg_type)
 
     # Increment counter by given amount and return its updated value.
     def increment(self, amount, do_print=False) -> int:
-        current_value = int(msg_queue.receive(type=self.MSG_TYPE)[0].decode())
+        current_value = int(msg_queue.receive(type=self.msg_type)[0].decode())
 
         if do_print:
             print("Counter value:", current_value)
         updated_value = current_value + amount
         self.value = updated_value
-        self.msg_queue.send(str(updated_value).encode(), type=self.MSG_TYPE)
+        self.msg_queue.send(str(updated_value).encode(), type=self.msg_type)
         if do_print:
             print("Updated counter val:", updated_value)
         return updated_value
@@ -114,27 +119,37 @@ class MsgQueueCounter:
         self.increment(value - self.value)
 
 # Sync objects
-atomic_counter = MsgQueueCounter(0, msg_queue)
+atomic_counter = MsgQueueCounter(0, msg_queue, 1000)
+giver_counter = MsgQueueCounter(0, msg_queue, 1001)
+taker_counter = MsgQueueCounter(0, msg_queue, 1002)
 
 class Home(Process):
     
-    # timestamp = 0 # Move it in parent process
 
-    def __init__(self, id: int, trade_policy: str, consumption_rate, production_rate):
+    def __init__(self, id: int, trade_policy: str, consumption, production, home_socket):
         super().__init__()
         self.id = id
-        self.consumption_rate = consumption_rate
-        self.production_rate = production_rate
+        self.consumption_rate = consumption
+        self.production_rate = production
         self.trade_policy = trade_policy
+        self.socket = home_socket
+
+    def market_transaction(self, type: str, energy_amount: int):
+        with self.socket:
+            self.socket.connect((LOCALHOST, MARKET_PORT))
+            self.market_transaction.sendall((f"{type} {energy_amount}").encode())
 
     def run(self):
         while True:
+            sync_all_homes(on_tick_start)
+
             energy_delta = self.production_rate - self.consumption_rate
             print(current_process().name + ":", f"D(E)= {energy_delta:.2f}", "[" + self.trade_policy + "]")
 
             if energy_delta > 0 and (self.trade_policy == Policy.GIVE_ONLY or \
                     self.trade_policy == Policy.SELL_IF_NONE):
                 # GIVER
+                giver_counter.increment(1)
                 givers_advertisment.clear() # Takers, if any, will be waiting for givers
             elif energy_delta < 0: # TAKER
                 awaiting_takers.clear() # Givers, if any, will be waiting for takers
@@ -144,8 +159,7 @@ class Home(Process):
             # TAKER
             if energy_delta < 0:
                 # Ask other homes for giveaway, wait for a reply, and if none, buy from market
-                c_b = atomic_counter.increment(1,True)
-                print(current_process().name, "enters loop, counter to", c_b)
+                taker_counter.increment(1)
                 taker.acquire()
                 while True:
                     givers_advertisment.wait()
@@ -159,16 +173,17 @@ class Home(Process):
                     except BusyError:
                         # All surplus advertisments were taken homes, buy on market
                         # TODO: Market transaction (always)
-                        print(current_process().name, "buys from market")
+                        print(current_process().name, "initiates buy from market")
+                        # self.market_transaction("BUY", -energy_delta)
                         break
                     surplus = float(surplus_advertisment.decode())
                     energy_received = min(surplus, -energy_delta) # energy_delta is negative
                     remaining_energy = surplus - energy_received
-                    print(f"\n Giver-{giver_id} now has", remaining_energy)
+                    # print(f"\n Giver-{giver_id} now has", remaining_energy)
 
                     # Put back in the msg queue only if the 'packet' still contains energy
                     if remaining_energy > 0:
-                        print("Putting back in queue: ", remaining_energy, f"from Giver-{giver_id}")
+                        # print("Putting back in queue: ", remaining_energy, f"from Giver-{giver_id}")
                         # Surplus from other home was enough to meed need of energy.
                         msg_queue.send(str(remaining_energy).encode(),type=giver_id)
                         break
@@ -176,27 +191,25 @@ class Home(Process):
                         energy_delta += energy_received
                          # Surplus from other home was insufficient, check for another one
                         continue
-                
-                c = atomic_counter.increment(-1,True)
-                print(current_process().name, "leaves loop, updated counter to", c)
-                if c == 0:
+                taker.release()
+                if taker_counter.increment(-1) == 0:
                     awaiting_takers.set()
 
-                taker.release()
 
             if energy_delta > 0:
 
                 if self.trade_policy == Policy.SELL_ONLY:
                     # TODO: market transaction
-                    print(current_process().name, "sells on market")
+                    print(current_process().name, "initiate sell on market")
+                    # self.market_transaction("SELL", -energy_delta)
                 else:
                     surplus_advertisment = f"{energy_delta}"
                     msg_queue.send(surplus_advertisment.encode(), type=self.id)
 
-                    givers_advertisment.set()
-                    print(current_process().name, "awaiting takers")
+                    if giver_counter.increment(-1) == 0:
+                        givers_advertisment.set()
+
                     awaiting_takers.wait()
-                    print(current_process().name, "passed barrier")
                     try:
                         remaining_energy = float(msg_queue.receive(type=self.id, block=False)[0].decode())
 
@@ -209,32 +222,38 @@ class Home(Process):
                         print(current_process().name, ": all surplus given")
                         pass
 
-            print(current_process().name, "reached sync point.")
-            sync_all_homes(on_tick_end)
-
+            # print(current_process().name, "reached sync point.")
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, handle_interrupt)
 
-    givers_advertisment.set()
-    awaiting_takers.set()
+    # givers_advertisment.set()
+    # awaiting_takers.set()
 
+    tick_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tick_socket.connect((LOCALHOST, MARKET_PORT))
+    print("Tick connection established")
 
     homes = []
     for i in range(HOMES_COUNT):
         production_rate = rand_range(MIN_PRODUCTION_RATE, MAX_PRODUCTION_RATE)
         consumption_rate = rand_range(MIN_CONSUMPTION_RATE, MAX_CONSUMPTION_RATE)
+        home_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        home_process = Home((i+1), Policy.getRandom(), production_rate, consumption_rate)
+        home_process = Home((i+1), Policy.getRandom(), production_rate, consumption_rate, home_socket)
         homes.append(home_process)
         home_process.start()
 
+    timestamp = 0
+    tick_notif = "NEXT_TICK".encode()
     while True:
         next_tick.wait()
         next_tick.clear()
 
-        print("~~", timestamp, "~~")
+        tick_socket.sendall(tick_notif)
+
         timestamp += 1
+        print("~~", timestamp, "~~")
 
     # for h in homes:
     #     h.join()
--- market.py
+++ market.py
@@ -1,9 +1,9 @@
-from socket import socket
-# from threading import Thread
+import socket
+from threading import Lock, Semaphore
 from concurrent.futures import ThreadPoolExecutor
 
 START_ENERGY_PRICE = 0.1
-MAX_CLIENTS = 3
+MAX_TRANSACTIONS = 3
 
 current_energy_price = START_ENERGY_PRICE  # in €/kWh
 
@@ -12,31 +12,67 @@ temperature = 25
 
 # -----------------
 
-on_going_transactions = 0
+on_going_transactions = Semaphore(MAX_TRANSACTIONS)
+transaction_lock = Lock()
 
-def handle_transaction(client_socket, address):
-    global on_going_transactions
-    on_going_transactions += 1
-    with client_socket:
-        print("Connected to client: ", address)
-        data = client_socket.recv(1024)
-        while len(data):
-            client_socket.sendall(data)
-            data = client_socket.recv(1024)
-        print("Disconnecting from client: ", address)
-    on_going_transactions -= 1
+energy_cost = 0
+GAMMA = 0.2
+previous_energy_cost = (1/GAMMA) * START_ENERGY_PRICE
+
+energy_bought = 0
+energy_sold = 0
+
+def handle_transaction(home_socket, address):
+    with home_socket:
+        print("Connected to home: ", address)
+
+        data = home_socket.recv(1024).decode()
+        transaction_type, amount = data.split(" ")
+        amount = float(amount)
+        print(transaction_type + " ends with", address)
+        
+        with transaction_lock:
+            if transaction_type == "SELL":
+                energy_bought += amount
+            else: # BUY
+                energy_sold += amount
+
+    on_going_transactions.release()
+
+def handle_tick(tick_socket):
+    print("Tick update thread started.")
+
+    global energy_cost
+    timestamp = 0
+    with tick_socket:
+        print("wait for tick update")
+        # Wait for tick update
+        while True:
+            timestamp += 1
+            print("~~", timestamp, "~~")
+            tick_notif = tick_socket.recv(1024)
+            if not len(tick_notif):
+                break
+            energy_cost = GAMMA * previous_energy_cost # [...] TODO: continue equation
 
 if __name__ == "__main__":
-
+    
     HOST = "localhost"
     PORT = 9000
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((HOST, PORT))
-        server_socket.listen(3)
-        # client_socket, address = server_socket.accept() # Wait for the first connection from a client
-        with ThreadPoolExecutor(max_workers=MAX_CLIENTS) as executor:
+        server_socket.listen(MAX_TRANSACTIONS)
+
+        # Establish connection for tick updates
+        tick_socket = server_socket.accept()[0]
+
+        with ThreadPoolExecutor(max_workers=MAX_TRANSACTIONS+1) as executor:
+            # Start a thread for tick updates
+            executor.submit(handle_tick, tick_socket)
+
             while True:
-                client_socket, address = server_socket.accept()
+                # on_going_transactions.acquire()
+                home_socket, address = server_socket.accept()
                 # handler = Thread(target=handle_transaction, args=(client_socket, address))
                 # handler.start()
-                future = executor.submit(handle_transaction)
+                executor.submit(handle_transaction, home_socket, address)
